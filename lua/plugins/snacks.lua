3db107389eb8b09f704dd9b7b73a744032597192
Tal <tal@server.fake>
2026-02-22 15:31:09 -0700

    Made notifications wrap

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		picker = {
+			win = {
+				preview = {
+					wo = {
+						wrap = true,
+						linebreak = true, -- optional, nicer wrapping
+					},
+				},
+			},
+		},
+	},
+}
