d7dc748ff8b81b16f6af3fcb17d3ec89ece5727e
tal <tal@vrify.com>
2026-02-27 15:38:12 -0700

    Disabled diagnostics in diff mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Disable diagnostics when in diff mode
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if vim.wo.diff then
+      vim.diagnostic.disable()
+    end
+  end,
+})
