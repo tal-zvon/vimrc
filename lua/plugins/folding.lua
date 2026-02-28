6e7799873beb5ad19f5acec8d4fe99533adc27cd
Tal <tal@server.fake>
2026-02-28 15:51:16 -0700

    Added nvim-ufo for nice folding

---
+++ lua/plugins/folding.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    event = "BufReadPost", -- Load when you open a file
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        -- Use LSP first, fallback to Treesitter, then Indent
+        return { "lsp", "indent" }
+      end,
+    },
+    config = function(_, opts)
+      -- Fold settings for nvim-ufo
+      vim.o.foldcolumn = "1" -- '0' is not show, '1' shows a small column
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Option: Custom handler to show line count at the end of the fold
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if curWidth + chunkWidth < targetWidth then
+              table.insert(newVirtText, { (" "):rep(targetWidth - curWidth - chunkWidth), "UfoFoldedEllipsis" })
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
+
+      opts.fold_virt_text_handler = handler
+      require("ufo").setup(opts)
+    end,
+  },
+}
