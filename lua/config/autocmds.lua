f7392d395084ae73359b39b6fb542b2fa94168bb
tal <tal@vrify.com>
2026-03-02 13:09:30 -0700

    Removed custom fold text

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,24 +16,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
--- Show nicer fold text in diff mode
-_G.MyFoldText = function()
-  local line_count = vim.v.foldend - vim.v.foldstart + 1
-  return " ---- " .. line_count .. " lines " .. string.rep("-", 50)
-end
-
-vim.api.nvim_create_autocmd({ "DiffUpdated", "WinEnter", "BufWinEnter" }, {
-  group = vim.api.nvim_create_augroup("DiffVisualCleanup", { clear = true }),
-  callback = function()
-    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-      if vim.api.nvim_win_is_valid(win) and vim.api.nvim_get_option_value("diff", { win = win }) then
-        vim.api.nvim_set_option_value("foldtext", "v:lua.MyFoldText()", { win = win })
-        vim.api.nvim_set_option_value("foldcolumn", "0", { win = win })
-      else
-        -- Revert for normal windows
-        vim.api.nvim_set_option_value("foldtext", "foldtext()", { win = win })
-      end
-    end
-  end,
-})
-
