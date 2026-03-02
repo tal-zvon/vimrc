c79e675403b6935e650a8ea50bcd87aca262277b
tal <tal@vrify.com>
2026-03-02 13:00:02 -0700

    Stop neovim from complaining about missing vim object

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
