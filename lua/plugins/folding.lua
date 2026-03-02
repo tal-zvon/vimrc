b3fbf141eaab47c7e3bb1d5c6e00676fcfb7a91b
tal <tal@vrify.com>
2026-03-02 13:15:20 -0700

    Made diff mode show different fold text

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -1,3 +1,49 @@
+local handler = function(virtText, lnum, endLnum, width, truncate, ctx)
+  local is_diff_window = vim.api.nvim_get_option_value("diff", { win = ctx.winid })
+
+  if is_diff_window then
+    local line_count = endLnum - lnum + 1
+    local text = (" ---- %d lines %s"):format(line_count, string.rep("-", 50))
+
+    return {
+      { truncate(text, width), "Folded" },
+    }
+  end
+
+  local newVirtText = {}
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+      if curWidth + chunkWidth < targetWidth then
+        table.insert(
+          newVirtText,
+          { (" "):rep(targetWidth - curWidth - chunkWidth), "UfoFoldedEllipsis" }
+        )
+      end
+      break
+    end
+
+    curWidth = curWidth + chunkWidth
+  end
+
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
+
 return {
   {
     "kevinhwang91/nvim-ufo",
@@ -16,34 +62,7 @@ return {
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
 
-      -- Option: Custom handler to show line count at the end of the fold
-      local handler = function(virtText, lnum, endLnum, width, truncate)
-        local newVirtText = {}
-        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-        local sufWidth = vim.fn.strdisplaywidth(suffix)
-        local targetWidth = width - sufWidth
-        local curWidth = 0
-        for _, chunk in ipairs(virtText) do
-          local chunkText = chunk[1]
-          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-          else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if curWidth + chunkWidth < targetWidth then
-              table.insert(newVirtText, { (" "):rep(targetWidth - curWidth - chunkWidth), "UfoFoldedEllipsis" })
-            end
-            break
-          end
-          curWidth = curWidth + chunkWidth
-        end
-        table.insert(newVirtText, { suffix, "MoreMsg" })
-        return newVirtText
-      end
-
+      -- Option: Custom handler to show line count at the end of the fold in diff mode
       opts.fold_virt_text_handler = handler
       require("ufo").setup(opts)
     end,
