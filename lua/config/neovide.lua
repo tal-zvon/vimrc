7b9abfe042ad295e3d0515fef6f1aaeb5a9a6f29
tal <tal@vrify.com>
2026-03-15 12:19:41 -0600

    Made neovide open my working dir

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -7,3 +7,25 @@ vim.g.neovide_scale_factor = 2.0
 vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<D-v>", function()
   vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
 end, { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- 1. Enable Auto-Draft Mode
+    local ok, lazyvim_utils = pcall(require, "plugins.utils.lazyvim")
+    if ok and lazyvim_utils.toggle_auto_draft then
+      lazyvim_utils.toggle_auto_draft()
+    end
+
+    -- 2. Open project and restore session
+    local project_path = vim.fn.expand("~/Documents/Old Sublime Tabs/")
+    if vim.fn.isdirectory(project_path) == 1 then
+      vim.api.nvim_set_current_dir(project_path)
+
+      -- Try to restore session
+      local has_persistence, persistence = pcall(require, "persistence")
+      if has_persistence then
+        persistence.load()
+      end
+    end
+  end,
+})
