f1ea0fcb004969b164dd2088b57c3876c17cd16a
tal <tal@vrify.com>
2026-04-05 18:06:33 -0600

    Removed Redundant Search-Open-Buffers Command
    Use <leader>sB

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -9,11 +9,6 @@ return {
         lazyvim_utils.toggle_clipboard,
         desc = "Toggle OS Clipboard",
       },
-      {
-        "<leader>m",
-        lazyvim_utils.search_open_buffers_content,
-        desc = "Search Open Buffers by Content",
-      },
       { "<leader>uu", lazyvim_utils.toggle_auto_draft, desc = "Toggle Auto-Draft Mode" },
     },
   },
