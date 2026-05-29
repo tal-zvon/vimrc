d5aa3ac18d679a258e8bf6815c3d498c9b59b133
Tal <tal@server.fake>
2026-05-29 01:20:04 -0600

    Added markdown rust syntax highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "markdown",
+        "markdown_inline",
+        "rust",
+      },
+    },
+  },
+}
